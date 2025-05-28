d75a9064f0bd15b507cb2f324004134b0862dcc5
Anuoluwapo <anuvictor2005@gmail.com>
2025-05-28 17:46:56 +0100

    jwt generator created

---
+++ server/utils/jwtGenerator.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config(); //allows us get access to all our environment variable
+
+function jwtGenerator(user_id) {
+  const payload = {
+    //create payload
+    user: user_id,
+  };
+
+  //signing the token
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: 60 * 60 }); //takes payload data and secret
+}
+
+module.exports = jwtGenerator;
