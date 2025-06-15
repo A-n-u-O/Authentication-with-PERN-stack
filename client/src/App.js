5c4009336586c6a39f2e288d4646c516b09f791c
Anuoluwapo <anuvictor2005@gmail.com>
2025-06-16 00:51:35 +0100

    register, dashboard and login routes configured | dashboard protected

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,43 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  // Redirect,
+  Navigate,
 } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <>
       <Router>
         <div className="container">
           <Routes>
-            {/* render prevents remount */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route exact path="/dashboard" element={<Dashboard />} />
+            {/* protecting dashboard  */}
+            <Route
+              path="/dashboard"
+              element={
+                isAuthenticated ? <Dashboard /> : <Navigate to="/login" />
+              }
+            />
+
+            <Route
+              path="/login"
+              element={
+                !isAuthenticated ? (
+                  <Login setIsAuthenticated={setIsAuthenticated} />
+                ) : (
+                  <Navigate to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              element={<Register setIsAuthenticated={setIsAuthenticated} />}
+            />
           </Routes>
         </div>
       </Router>
